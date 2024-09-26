0fb0e92eb0b430b897c80c1e3f167ee20d8304a9
garceteclaudio <garcete.claudio@gmail.com>
2024-09-26 03:23:46 -0300

    fix puntaje

--- src/scenes/Escena1.js
+++ src/scenes/Escena1.js
@@ -4,6 +4,9 @@ export default class Escena1 extends Phaser.Scene {
     this.jugador = null;
     this.grupoMeteoros = null;
     this.cursors = null;
+    this.teclas = null; // propiedad para las teclas WASD
+    this.puntaje = 0;
+    this.textoDePuntaje = null; // Cambiado a null
   }
 
   preload() {
@@ -15,9 +18,9 @@ export default class Escena1 extends Phaser.Scene {
   create() {
     this.add.image(400, 300, "cielo");
     this.jugador = this.physics.add.sprite(400, 550, "nave");
-    this.jugador.setCollideWorldBounds(true); //evita q salga de la pantalla
+    this.jugador.setCollideWorldBounds(true); // evita que salga de la pantalla
 
-    this.grupoMeteoros = this.physics.add.group(); //creando el grupo de meteoritos
+    this.grupoMeteoros = this.physics.add.group(); // creando el grupo de meteoritos
 
     this.time.addEvent({
       delay: 1000,
@@ -26,7 +29,37 @@ export default class Escena1 extends Phaser.Scene {
       loop: true,
     });
 
+    // Evento para incrementar el puntaje cada segundo
+    this.time.addEvent({
+      delay: 1000,
+      callback: this.incrementarPuntaje,
+      callbackScope: this,
+      loop: true,
+    });
+
+    // Crear las teclas de flechas
     this.cursors = this.input.keyboard.createCursorKeys();
+
+    // Crear teclas WASD
+    this.teclas = this.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      down: Phaser.Input.Keyboard.KeyCodes.S,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+    });
+
+    this.physics.add.collider(
+      this.jugador,
+      this.grupoMeteoros,
+      this.gameOver,
+      null,
+      this
+    );
+
+    this.textoDePuntaje = this.add.text(16, 16, "Puntaje: 0", {
+      fontSize: "32px",
+      fill: "#fff",
+    });
   }
 
   generarMeteoros() {
@@ -36,13 +69,38 @@ export default class Escena1 extends Phaser.Scene {
     meteoro.setVelocityY(200);
   }
 
-  update() {
-    this.jugador.setVelocityX(0);
+  incrementarPuntaje() {
+    this.puntaje += 1; // Incrementar el puntaje
+    this.textoDePuntaje.setText(`Puntaje: ${this.puntaje}`); // Actualizar el texto del puntaje
+  }
 
-    if (this.cursors.left.isDown) {
+  gameOver(jugador) {
+    this.physics.pause();
+    jugador.setTint(0xff0000);
+    console.log("Game over");
+  }
+
+  update() {
+    this.jugador.setVelocity(0); // Resetea las velocidades antes de actualizar
+
+    // Movimiento hacia la izquierda
+    if (this.cursors.left.isDown || this.teclas.left.isDown) {
       this.jugador.setVelocityX(-300);
-    } else if (this.cursors.right.isDown) {
+    }
+
+    // Movimiento hacia la derecha
+    if (this.cursors.right.isDown || this.teclas.right.isDown) {
       this.jugador.setVelocityX(300);
+    }
+
+    // Movimiento hacia arriba
+    if (this.cursors.up.isDown || this.teclas.up.isDown) {
+      this.jugador.setVelocityY(-300);
+    }
+
+    // Movimiento hacia abajo
+    if (this.cursors.down.isDown || this.teclas.down.isDown) {
+      this.jugador.setVelocityY(300);
     }
   }
 }
